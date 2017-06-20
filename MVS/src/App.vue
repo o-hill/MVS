61ce24f9ed67c2c519c23971bdb09cc8db7a40cb
Oliver <oliverhi@umich.edu>
2017-06-20 14:27:49 -0400

    Basic vue page with the webcam stream in the middle.  Currently unformatted.

---
+++ MVS/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+	  <img src="./assets/logo.png">
+	  <img width = "650" height = "350"
+		  src = "http://0.0.0.0:5000/video_feed">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
